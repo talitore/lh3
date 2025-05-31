d875787303e02a518438e3a8011b5096cb716878
Taylor Kems <tekems@gmail.com>
2025-05-31 15:23:22 -0500

    reset

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-// console.log(`E2E_TESTING_MODE on server: ${process.env.E2E_TESTING_MODE}`); // Debug log
-import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/auth';
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
