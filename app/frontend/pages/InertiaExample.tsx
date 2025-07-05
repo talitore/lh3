5475590c23188c5108be3635f5d814fb537aa50a
Taylor Kems <tekems@gmail.com>
2025-07-05 10:22:44 -0500

    rm out of date docs and unused import

--- app/frontend/pages/InertiaExample.tsx
+++ app/frontend/pages/InertiaExample.tsx
@@ -2,7 +2,6 @@ import { Head } from '@inertiajs/react';
 import { useState } from 'react';
 
 import inertiaSvg from '/assets/inertia.svg';
-import reactSvg from '/assets/react.svg';
 import viteRubySvg from '/assets/vite_ruby.svg';
 
 import cs from './InertiaExample.module.css';
