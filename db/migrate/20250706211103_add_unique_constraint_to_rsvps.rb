d6437bcb8caa0c26a3761e3095402da12bc709cd
Taylor Kems <tekems@gmail.com>
2025-07-06 16:11:51 -0500

    feat: add unique constraint to prevent duplicate RSVPs

---
+++ db/migrate/20250706211103_add_unique_constraint_to_rsvps.rb
@@ -0,0 +1,9 @@
+class AddUniqueConstraintToRsvps < ActiveRecord::Migration[8.0]
+  def up
+    add_index :rsvps, [:user_id, :event_id], unique: true, name: 'index_rsvps_on_user_id_and_event_id'
+  end
+
+  def down
+    remove_index :rsvps, name: 'index_rsvps_on_user_id_and_event_id'
+  end
+end
