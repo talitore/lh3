28d09404b93aff2b18757d601bee3ef002a9eca6
Taylor Kems <tekems@gmail.com>
2025-07-06 09:21:39 -0500

    feat: tailwind setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import RubyPlugin from 'vite-plugin-ruby';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    RubyPlugin(),
-  ],
-})
+  plugins: [react(), RubyPlugin(), tailwindcss()],
+});
